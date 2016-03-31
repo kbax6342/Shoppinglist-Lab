c5d235c08841d52f59f6aebabd96e64e032b91ff
Kevin Baxter <kbax6342@gmail.com>
2016-03-30 20:37:40 -0400

    Shoppinglist Lab

---
+++ shoppinglist.js
@@ -0,0 +1,30 @@
+var myObj = [
+    {Food:"Milk", 
+     Price: 2.50},
+    
+    {Food:"Bread", 
+     Price: 4.00},
+    
+
+    {Food: "Cheese", 
+     Price: 3.00}
+];
+
+myObj.forEach(function(myShopping){
+    console.log(myShopping.Food + " " + myShopping.Price);
+
+});
+
+var price = 
+function(total){
+    
+}
+
+//var str1 = console.log(myShopping.Food);
+//var str2 = console.log(myShopping.Price);
+//consol.log(str1.concat(str2);) 
+
+//function(total) = {
+//    2.50 + 4.00 + 3.00
+//};
+//console.log("Total: " + total)
